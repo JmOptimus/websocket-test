eda04cfefad60fb7e1a4a61fd611792ab790105c
JmOptimus <44059799+JmOptimus@users.noreply.github.com>
2026-04-15 19:54:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,14 @@
-import asyncio
-import http
-import signal
+from typing import Optional
 
-import websockets
+from fastapi import FastAPI
 
-
-async def echo(websocket):
-    async for message in websocket:
-      await websocket.send(message)
+app = FastAPI()
 
 
-async def health_check(path, request_headers):
-  if path == "/healthz":
-    return http.HTTPStatus.OK, [], b"OK\n"
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-async def main():
-  loop = asyncio.get_running_loop()
-  stop = loop.create_future()
-  loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-
-  async with websockets.serve(
-    echo,
-    host="",
-    port="8080",
-    process_request=health_check,
-  ):
-    await stop
-
-
-if __name__ == "__main__":
-  asyncio.run(main())
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
